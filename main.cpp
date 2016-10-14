355abc13a979adc74a9665f4b0bd99d45ca140ee
Muhamad Fakhrusy <v8areu@gmail.com>
2016-10-14 09:32:35 +0700

    change vector to fixed size

--- main.cpp
+++ main.cpp
@@ -19,8 +19,8 @@ class TDMA_SOLVER_DIRICHLET: public Parameters {
 
 	private:
 		const int N_nodes_const = N_nodes;
-		std::vector<std::vector<double>> temperature_time;	
-		std::vector<double> temperature_spatial;
+		//std::vector<std::vector<double>> temperature_time;	
+		//std::vector<double> temperature_spatial;
 		double error_computation;
 
 		//compute A constant
@@ -34,7 +34,6 @@ class TDMA_SOLVER_DIRICHLET: public Parameters {
 		}
 
 
-
 	public:
 		std::vector<double> temperature_computation (double alpha, 
 				double delta_t, 
@@ -45,73 +44,87 @@ class TDMA_SOLVER_DIRICHLET: public Parameters {
 				int N_iter_max,
 				int N_nodes_const) {
 
+			std::vector<double> temperature_spatial(N_nodes_const);
+			std::vector<std::vector<double>> temperature_time(N_iter_max, std::vector<double>(N_nodes_const));
+
 			double B = B_constant_computation(alpha, delta_t, delta_x);
 			double A = A_constant_computation(alpha, delta_t, delta_x);
 
-
 			//initialize the vector by initial conditions 
-			temperature_spatial.push_back(temperature_initial);
+			temperature_spatial[0] = temperature_initial;
 			for (int i=1; i<N_nodes_const-1; i++) {
-				temperature_spatial.push_back(0);
+				temperature_spatial[i] = 0.;
 			}
-			temperature_spatial.push_back(temperature_final);
-
+			temperature_spatial[N_nodes_const-1] = temperature_final;
+			//temperature_spatial.push_back(temperature_initial);
+			//for (int i=1; i<N_nodes_const-1; i++) {
+			//	temperature_spatial.push_back(0);
+			//}
+			//temperature_spatial.push_back(temperature_final);
 
 
 			//initialize the temperature_time
-			temperature_time.push_back(temperature_spatial);
+			//temperature_time.push_back(temperature_spatial);
+			for (int i=0; i<N_nodes_const; i++) {
+				temperature_time[0][i] = temperature_spatial[i];
+			}
 
 			//looping process
-			double time_count = 0;
-			double error_computation = 1;
-			while (error_computation > error_max) {
-
+			int time_count = 0;
+			double error_computation = 0.;
+			do {
 
 				//clear the temperature_spatial
-				temperature_spatial.clear();
+				//temperature_spatial.clear();
+				for (int i=0; i<N_nodes_const; i++) {
+					temperature_spatial[i] = 0.;
+				}
 
 				//invoke boundary condition at initial
-				temperature_spatial.push_back(temperature_initial);
-				
-				//compute the 1-st node (initial = 0-th node!)
-				double temp_temperature;
-				temp_temperature = (temperature_initial - B*temperature_initial)/(-A);
-				temperature_spatial.push_back(temp_temperature);
-				
-				//looping process
-				for (int i = 2; i<N_nodes_const; i++) {
-					temp_temperature = (temperature_time[time_count][2] + A*temperature_spatial[i-1] - B*temperature_spatial[i])/(-A);
-				}
-
-
+				//temperature_spatial.push_back(temperature_initial);
+				temperature_spatial[0] = temperature_initial;
 
 				//invoke boundary condition at final
-				temperature_spatial.push_back(temperature_final);
-				
-				std::cout << time_count << " INI TIME KE SEKINI " << std::endl;
-				for (int i=0; i<N_nodes_const; i++) {
-					std::cout << i << " " << temperature_spatial[i] << std::endl;
+				//temperature_spatial.push_back(temperature_final);
+				temperature_spatial[N_nodes_const-1] = temperature_final;
+
+				//compute the 1-st node (initial = 0-th node!)
+				//temperature_spatial.push_back(temp_temperature);
+				temperature_spatial[1] = (temperature_initial - B*temperature_initial)/(-A);
+
+				//looping process
+				for (int i = 1; i<N_nodes_const-2; i++) {
+					//temperature_spatial.push_back(temp_temperature);
+					temperature_spatial[i+1] = (temperature_time[time_count][i] + A*temperature_spatial[i-1] - B*temperature_spatial[i])/(-A);					
 				}
 
-				//add it to temperature_time
-				temperature_time.push_back(temperature_spatial);
-				error_max = 0;
-				for (int i = 0; i <N_nodes_const; i++) {
-					error_max = (error_max + temperature_spatial[i])/N_nodes_const;		
-				}
 				time_count = time_count + 1;
+				//add it to temperature_time
+				//temperature_time.push_back(temperature_spatial);
+				for (int i=0; i<N_nodes_const; i++) {
+					temperature_time[time_count][i] = temperature_spatial[i];
+					std::cout << temperature_spatial[i] << "\t";
+				}
+				std::cout << "\n";
+
+
+				for (int i = 0; i <N_nodes_const; i++) {
+					double temp = abs(temperature_time[time_count][i] - temperature_time[time_count - 1][i]);
+					error_computation = (error_computation + temp);		
+				}
+				error_computation = error_computation/N_nodes_const;
+			
+			
 				if (time_count > 10) {
 					std::cout << "GAGAL" << std::endl;
 					break;
 				}
-			};
 
+			} while (error_computation > error_max);
 
 			std::vector<double> temperature = temperature_spatial;
 			return temperature;
 		}
-
-
 };
 
 
@@ -123,7 +136,7 @@ int main() {
 	par_1.delta_t			=	0.05;
 	par_1.delta_x			=	0.1;
 	par_1.error_max			=	0.1;
-	par_1.temperature_initial	=	0;
+	par_1.temperature_initial	=	1;
 	par_1.temperature_final		=	150;
 	par_1.N_iter_max		=	100;
 	par_1.N_nodes			=	10;
@@ -138,10 +151,5 @@ int main() {
 	}
 	std::cout << std::endl;
 
-
-
-
-
-	
 
 }
